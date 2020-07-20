e2f5b1bdd3c8f5c4a2b9bb4dc4124d8d8ad9ee64
Akash Joshi <>
2020-07-20 19:03:15 +0530

    Add pre-commit Hooks

--- server/middleware/renderer.js
+++ server/middleware/renderer.js
@@ -7,7 +7,7 @@ import App from "../../src/App";
 const path = require("path");
 const fs = require("fs");
 
-export default (req, res, next) => {
+export default (req, res) => {
   // point to the html file created by CRA's build tool
   const filePath = path.resolve(__dirname, "..", "..", "build", "index.html");
 

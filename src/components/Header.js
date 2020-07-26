5c99840d1d644f137c55938b58c2ab6dcd36096b
Akash Joshi <akash.joshi@viit.ac.in>
2020-07-26 20:06:01 +0530

    Fix styled-components warning

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function Header({ hnOrange }) {
-  const HeaderStyles = styled.header`
-    display: grid;
-    grid-template-columns: 80px 45px 55px 1fr;
-    color: black;
-    background: ${hnOrange};
-    padding: 0.5em;
-    font-size: 0.9em;
-    @media (max-width: 700px) {
-      grid-template-columns: 75px 55px 1fr;
-    }
-  `;
+const HeaderStyles = styled.header`
+  display: grid;
+  grid-template-columns: 80px 45px 55px 1fr;
+  color: black;
+  background: ${props => props.hnOrange};
+  padding: 0.5em;
+  font-size: 0.9em;
+  @media (max-width: 700px) {
+    grid-template-columns: 75px 55px 1fr;
+  }
+`;
 
+export default function Header({ hnOrange }) {
   return (
-    <HeaderStyles>
+    <HeaderStyles hnOrange={hnOrange}>
       <span>Comments</span>
       <span className="hide-on-mobile">Vote Count</span>
       <span>Upvote</span>
